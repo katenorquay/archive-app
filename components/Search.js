53af03b71a22afffd63e3b72dad4fc4f76b561ff
Kate Norquay <katenorquay@live.com>
2016-12-20 09:26:56 +1300

    adds second form to search page

--- components/Search.js
+++ components/Search.js
@@ -3,10 +3,18 @@ import Header from './Header'
 import callAPI from '../callAPI'
 
 function Search({state, dispatch}) {
-  function BeginSearch(e) {
+  function urlSearch(e) {
     e.preventDefault()
     var userInput = {
       url: document.getElementById('url').value
+    }
+    callAPI(dispatch, userInput, state)
+  }
+
+  function yearSearch(e) {
+    e.preventDefault()
+    var userInput = {
+      year: document.getElementById('year').value
     }
     callAPI(dispatch, userInput, state)
   }
@@ -16,11 +24,15 @@ function Search({state, dispatch}) {
     <Header />
     <div className='info'>
       <h3>How It Works:</h3>
-      <p>Enter a url, the years you are interested in seeing that site. This app will find screenshots archived on the wayback machine to show you a quick picture of your site through out time</p>
+      <p>Enter a url that you are interested in seeing throughout time, or a time period that you want to see. This app will use the wayback machine to show archived screenshots of your desired site/time period</p>
     </div>
     <form>
      <input placeholder='Enter Url' type='text' id='url'></input>
-     <input type='submit' value='submit' onClick={BeginSearch}/>
+     <input type='submit' value='submit' onClick={urlSearch}/>
+    </form>
+    <form>
+     <input placeholder='Enter Year' type='text' id='year'></input>
+     <input type='submit' value='submit' onClick={yearSearch}/>
     </form>
   </div>
 )}

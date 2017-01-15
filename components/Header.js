5a9b4d9a5e13920fdc84ace918fefd13f96d1c27
katenorquay <katenorquay@live.com>
2017-01-15 23:13:27 +1300

    linked up social media buttons

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ module.exports = (props) =>
 <div className='header'>
   <img src='http://static.tumblr.com/uhg4bc4/50wm2srxg/the-design-nerd_smallest.jpg'></img>
   <div className='header-buttons'>
-    <a className='btn'>Home</a>
-    <a className='btn'>Contact</a>
-    <a className='btn'>Github</a>
+    <a href='/' className='btn'>Home</a>
+    <a href='https://twitter.com/norquaykate?lang=en' className='btn'>Tweet Me</a>
+    <a href='https://github.com/katenorquay' className='btn'>Github</a>
   </div>
 </div>
